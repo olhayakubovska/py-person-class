a2efde030f09e9b9bcb05a5b120345eac9692a76
Olga <oyakubovska80@gmail.com>
2026-09-16 17:15:51 +0200

    Solution

--- app/main.py
+++ app/main.py
@@ -7,20 +7,16 @@ class Person:
 
 
 def create_person_list(people: list) -> list:
-    arr = []
     for item in people:
         Person.people[item["name"]] = Person(item["name"], item["age"])
 
     for item in people:
-        new_person = Person(item["name"], item["age"])
-
         new_person = Person.people[item["name"]]
 
-        if item.get("wife") and item["wife"] is not None:
+        if item.get("wife"):
             new_person.wife = Person.people[item["wife"]]
 
-        if item.get("husband") and item["husband"] is not None:
+        if item.get("husband"):
             new_person.husband = Person.people[item["husband"]]
 
-        arr.append(new_person)
-    return arr
+    return [Person.people[item["name"]] for item in people]
